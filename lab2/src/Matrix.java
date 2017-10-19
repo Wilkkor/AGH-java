1ea05672d141a098d1a9cf6228d467fe5027f75d
Wilkkor <kornelwil@gmail.com>
2017-10-19 10:47:29 +0200

    first lab1 all

--- lab2/src/Matrix.java
+++ lab2/src/Matrix.java
@@ -1,5 +1,13 @@
 
 
 public class Matrix {
-    
+    double[]data;
+    int rows;
+    int cols;
+    //...
+    Matrix(int rows, int cols){
+        this.rows = rows;
+        this.cols = cols;
+        data = new double[rows*cols];
+    }
 }
