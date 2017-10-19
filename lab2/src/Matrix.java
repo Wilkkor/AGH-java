b3db6e656a3dc70cedb3612dc699208e985059ce
kwilk <kornelwil@gmail.com>
2017-10-19 13:54:23 +0200

    lab2

--- lab2/src/Matrix.java
+++ lab2/src/Matrix.java
@@ -1,10 +1,9 @@
 
 
 public class Matrix {
-    double[]data;
-    int rows;
-    int cols;
-    //...
+    private double[]data;
+    private int rows;
+    private int cols;
     Matrix(int rows, int cols){
         this.rows = rows;
         this.cols = cols;
@@ -22,13 +21,108 @@ public class Matrix {
         data=new double[rows*cols];
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
-                if(j>d[i].length){
-                    data[cols*i+j]=0;
-                }
-                else {
+                if(j<d[i].length){
                     data[cols*i+j]=d[i][j];
+                }else {
+                    data[cols*i+j]=0;
                 }
             }
         }
     }
+    double[][] asArray(){
+        double[][] a=new double[rows][cols];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                a[i][j]=data[cols*i+j];
+            }
+        }
+        return a;
+    }
+    double get(int r,int c){
+        return data[r*cols+c];
+    }
+    double getrows(){
+        return rows;
+    }
+    double getcols(){
+        return cols;
+    }
+    void set (int r,int c, double value){
+        data[r*cols+c]=value;
+    }
+    public String toString(){
+        StringBuilder buf = new StringBuilder();
+        buf.append("[");
+        for(int i=0;i<rows;i++){
+            buf.append("[");
+            for(int j=0;j<cols;j++){
+                buf.append(data[i*cols+rows]);
+                buf.append(',');
+            }
+            buf.deleteCharAt(buf.length()-1);
+            buf.append(']');
+        }
+        return buf.toString();
+    }
+    void reshape(int newRows,int newCols){
+        if(rows*cols != newRows*newCols)
+            throw new RuntimeException(String.format("%d x %d matrix can't be reshaped to %d x %d",rows,cols,newRows,newCols));
+        rows=newRows;
+        cols=newCols;
+    }
+    int[] shape(){
+        int tab[]=new int[2];
+        tab[0]=rows;
+        tab[1]=cols;
+        return tab;
+    }
+    Matrix add(Matrix m)
+    {
+        if(m.getcols()!=cols||m.getrows()!=rows)
+            throw new RuntimeException(String.format("%d x %d matrix can't be added to %d x %d",rows,cols,m.getrows(),m.getcols()));
+        Matrix W=new Matrix(rows,cols);
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                W.set(i,j,data[i*cols+j]+m.get(i,j));
+            }
+        }
+        return W;
+    }Matrix sub(Matrix m)
+    {
+        if(m.getcols()!=cols||m.getrows()!=rows)
+            throw new RuntimeException(String.format("%d x %d matrix can't be substracted to %d x %d",rows,cols,m.getrows(),m.getcols()));
+        Matrix W=new Matrix(rows,cols);
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                W.set(i,j,data[i*cols+j]-m.get(i,j));
+            }
+        }
+        return W;
+    }
+    Matrix mul(Matrix m)
+    {
+        if(m.getcols()!=cols||m.getrows()!=rows)
+            throw new RuntimeException(String.format("%d x %d matrix can't be multiplied to %d x %d",rows,cols,m.getrows(),m.getcols()));
+        Matrix W=new Matrix(rows,cols);
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                W.set(i,j,data[i*cols+j]*m.get(i,j));
+            }
+        }
+        return W;
+    }
+    Matrix div(Matrix m)
+    {
+        if(m.getcols()!=cols||m.getrows()!=rows)
+            throw new RuntimeException(String.format("%d x %d matrix can't be divided to %d x %d",rows,cols,m.getrows(),m.getcols()));
+        Matrix W=new Matrix(rows,cols);
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                W.set(i,j,data[i*cols+j]/m.get(i,j));
+            }
+        }
+        return W;
+    }
+    
+
 }
--- lab2/src/Matrixtest.java
+++ lab2/src/Matrixtest.java
@@ -3,14 +3,37 @@ public class Matrixtest {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("podaj rows i cols");
-        int rows=2,cols=3;
-        double[][] tab={{1,1,1},{1,1,1},{1,1,1}};
+        double[][] tab={{1,2,3,4},{5,6,7,8},{9,10,11,12}};
         Matrix A=new Matrix(tab);
-        for(int i=0;i<rows;i++){
-            for(int j=0;j<cols;j++){
-                System.out.printf("%f ",A.data[A.rows*i+j]);
+        //System.out.println(A.get(3,3));
+        System.out.println("rows"+A.getrows()+" cols "+A.getcols());
+        for(int i=0;i<A.getrows();i++){
+            for(int j=0;j<A.getcols();j++){
+                System.out.printf("%f ",A.get(i,j));
             }
             System.out.println("");
         }
+        double[][] a=A.asArray();
+        System.out.println("teraz wypisze tablicę");
+        for(int i=0;i<A.getrows();i++){
+            for(int j=0;j<A.getcols();j++){
+                System.out.printf("%f ",a[i][j]);
+            }
+            System.out.println("");
+        }
+        for(int i=0;i<A.getrows();i++){
+            for(int j=0;j<A.getcols();j++){
+                A.set(i,j,100.0);
+            }
+        }
+        System.out.println("teraz macierz setek");
+        for(int i=0;i<A.getrows();i++){
+            for(int j=0;j<A.getcols();j++){
+                System.out.printf("%f ",A.get(i,j));
+            }
+            System.out.println("");
+        }
+        System.out.println(A.toString());
+
     }
 }
