ac5f48c0c7cb1cba28dfbe821e75ffa7210552d1
Wilkkor <kornelwil@gmail.com>
2017-10-26 14:36:37 +0200

    lab2 all but tests

---
+++ lab2/test/MatrixTest.java
@@ -0,0 +1,86 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MatrixTest {
+    @Test
+    public void asArray() throws Exception {
+    }
+
+    @Test
+    public void print() throws Exception {
+    }
+
+    @Test
+    public void get() throws Exception {
+    }
+
+    @Test
+    public void getrows() throws Exception {
+    }
+
+    @Test
+    public void getcols() throws Exception {
+    }
+
+    @Test
+    public void set() throws Exception {
+    }
+
+    @Test
+    public void testtoString() throws Exception {
+    }
+
+    @Test
+    public void reshape() throws Exception {
+    }
+
+    @Test
+    public void shape() throws Exception {
+    }
+
+    @Test
+    public void add() throws Exception {
+    }
+
+    @Test
+    public void sub() throws Exception {
+    }
+
+    @Test
+    public void mul() throws Exception {
+    }
+
+    @Test
+    public void div() throws Exception {
+    }
+
+    @Test
+    public void frobenius() throws Exception {
+    }
+
+    @Test
+    public void multip() throws Exception {
+    }
+
+    @Test
+    public void random() throws Exception {
+    }
+
+    @Test
+    public void eye() throws Exception {
+    }
+
+    @Test
+    public void addRows() throws Exception {
+    }
+
+    @Test
+    public void swapRows() throws Exception {
+    }
+
+    @Test
+    public void inv() throws Exception {
+    }
+
+}
