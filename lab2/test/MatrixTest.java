f9f69540b9230ff0835dbadaa292431ec52636ff
Wilkkor <kornelwil@gmail.com>
2017-10-26 14:43:53 +0200

    lab2 all but tests

--- lab2/test/MatrixTest.java
+++ lab2/test/MatrixTest.java
@@ -1,10 +1,12 @@
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import static org.junit.Assert.*;
 
 public class MatrixTest {
     @Test
     public void asArray() throws Exception {
+        
     }
 
     @Test
