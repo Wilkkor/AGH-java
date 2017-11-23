33b08c8363f8e9e1e042cf3d88774c6718e2908f
kwilk <kornelwil@gmail.com>
2017-11-23 14:00:01 +0100

    Merge branch 'master' of https://github.com/Wilkkor/programowanie-obiektowe
    # Conflicts:
    #	lab2/src/Matrixtest.java

--- lab5/src/CSVReader.java
+++ lab5/src/CSVReader.java
@@ -1,23 +1,58 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CSVReader {
     BufferedReader reader;
     String delimiter;
     boolean hasHeader;
-
-    /**
-     *
-     * @param filename - nazwa pliku
-     * @param delimiter - separator pól
-     * @param hasHeader - czy plik ma wiersz nagłówkowy
-     */
-
-    public CSVReader(String filename,String delimiter,boolean hasHeader) {
-        reader = new BufferedReader(new FileReader(filename));
+    List<String> columnLabels = new ArrayList<>();
+    Map<String,Integer> columnLabelsToInt = new HashMap<>();
+    String[]current;
+    public CSVReader(String filename,String delimiter,boolean hasHeader) throws IOException {
+        this(new FileReader(filename),delimiter,true);
+    }
+    public CSVReader(String filename,String delimiter) throws IOException {
+        this(filename,delimiter,true);
+    }
+    public CSVReader(String filename) throws IOException {
+        this(filename,",",true);
+    }
+    public CSVReader(Reader read, String delimiter, boolean hasHeader) throws IOException {
+        reader = new BufferedReader(read);
         this.delimiter = delimiter;
         this.hasHeader = hasHeader;
-        if(hasHeader)parseHeader();
+        if (hasHeader) {
+            parseHeader();
+        }
+        next();
     }
-    //...
+    void parseHeader() throws IOException {
+        String line  = reader.readLine();
+        if(line==null){
+            return;
+        }
+        String[]header = line.split(delimiter);
+        for(int i=0;i<header.length;i++){
+            columnLabels.add(header[i]);
+            columnLabelsToInt.put(header[i],i);
+        }
+    }
+    boolean next() throws IOException {
+        String line  = reader.readLine();
+        if(line==null){
+            return false;
+        }
+        String[]current = line.split(delimiter);
+        current=new String[100];
+        return false;
+    }
+    String get(int i){
+        return current[i];
+    }
+    String get(String label){
+        return current[columnLabelsToInt.get(label)];
+    }
 }
